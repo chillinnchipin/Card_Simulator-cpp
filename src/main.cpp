63bf2c85e8049d4e74f3f150e30dbdfec6d57acc
Chip Hardy <chiphardy1995@icloud.com>
2026-07-18 14:41:56 -0500

    added warning message to inform the main is temporary and does not perform any function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
 int main(int argc, const char* argv[]) {
+    cout << "Usage not implemented" << endl
+        << "this is a temporary main file to ensure compilation" << endl;
     return 0;
 }
