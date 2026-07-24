e686d544a02f6d3f3ab47bd6e31599d8fa61ea9f
Chip Hardy <chiphardy1995@icloud.com>
2026-07-24 14:57:57 -0500

    added initial testing outline for Deck class

---
+++ test/test_deck.cpp
@@ -0,0 +1,78 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Automated.h>
+#include <CUnit/Basic.h>
+
+#include <string>
+#include <array>
+#include <stdlib.h>
+#include <stdexcept>
+
+#include "../headers/card.hpp"
+
+int setup_Deck_test_suite()
+{
+    // Setup code for the Deck test suite
+    return 0;
+}
+
+void test_Deck_default_constructor() {}
+
+void test_Deck_array_constructor() {}
+
+void test_Deck_vector_constructor() {}
+
+void test_size() {}
+
+void test_empty() {}
+
+void test_iteration() {}
+
+void test_get_deck_vector() {}
+
+void test_get_deck_array() {}
+
+void test_get_card_obj() {}
+
+void test_get_card() {}
+
+void test_get_cards_obj_array() {}
+
+void test_get_cards_obj_vector() {}
+
+void test_get_cards_array() {}
+
+void test_get_cards_vector() {}
+
+void test_set_deck() {}
+
+void test_set_deck_array() {}
+
+void test_set_deck_vector() {}
+
+void test_add_card_obj() {}
+
+void test_add_card() {}
+
+void test_append_card_obj() {}
+
+void test_append_card() {}
+
+void test_prepend_card_obj() {}
+
+void test_prepend_card() {}
+
+void test_remove_card() {}
+
+void test_pop_card() {}
+
+void test_peek_card() {}
+
+void test_pop_head() {}
+
+void test_peek_head() {}
+
+void test_clear() {}
+
+void test_sort() {}
+
+void test_shuffle() {}
